dfd0c3b137726283e627780d23d2c5e3aa53f249
piatkos <61123393+piatkos@users.noreply.github.com>
2021-03-02 10:35:27 +0100

    Update Settings.ps1
    I spread the comment about UNICODE

--- Settings.ps1
+++ Settings.ps1
@@ -1,6 +1,6 @@
 # Configure the variables below that will be used in the script
 $HAToken = "<Insert token>" # Example: eyJ0eXAiOiJKV1...
-$UserName = "<UserName>" # When not sure, open a command prompt and type: echo %USERNAME%
+$UserName = "<UserName>" # When not sure, open a command prompt and type: echo %USERNAME%, UNICODE characters can be as for example "Pi$([char]0x0105)tek" (Piątek)
 $HAUrl = "<HAUrl>" # Example: https://yourha.duckdns.org
 
 # Set language variables below
